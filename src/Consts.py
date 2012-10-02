0b24f8f33f6607b61ff109dc0d9daa879f0ab319
ydelia <ydelia@af7e6fdf-9444-4557-becd-b83b53ba5661>
2012-10-02 19:11:08 +0000

    Bump version number already. Fix odd resizing on "New calibration".
    git-svn-id: svn+ssh://source.gm.eurac.edu/DrawingRecorder/trunk@32 af7e6fdf-9444-4557-becd-b83b53ba5661

--- src/Consts.py
+++ src/Consts.py
@@ -8,7 +8,7 @@ from PyQt4 import QtGui, QtCore
 # Basic versioning
 APP_ORG         = "EURAC"               # application organization
 APP_NAME        = "DrawingRecorder"     # application name
-APP_VERSION     = "1.0"                 # application version
+APP_VERSION     = "1.1"                 # application version
 FORMAT_VERSION  = "1.0"                 # file format version
 APP_DELAY       = 1. / 20.              # general refresh delay for lengthy operations
 
