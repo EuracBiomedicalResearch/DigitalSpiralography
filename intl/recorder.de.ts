8e12a938ef39dce5e956bef7d76d2317f20cdb3f
ydelia <ydelia@af7e6fdf-9444-4557-becd-b83b53ba5661>
2013-02-14 21:17:54 +0000

    Adding German translation.
    git-svn-id: svn+ssh://source.gm.eurac.edu/DrawingRecorder/trunk@65 af7e6fdf-9444-4557-becd-b83b53ba5661

---
+++ intl/recorder.de.ts
@@ -0,0 +1,422 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="de" sourcelanguage="">
+<context>
+    <name>EndRecordingDialog</name>
+    <message>
+        <location filename="ui/endrecording.ui" line="20"/>
+        <source>Recording results</source>
+        <translation>Aufnahmeergebnisse</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="32"/>
+        <source>Patient ID:</source>
+        <translation>Patienten ID:</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="42"/>
+        <source>File:</source>
+        <translation>Datei:</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="54"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="63"/>
+        <source>Date/Time:</source>
+        <translation>Datum/Uhrzeit:</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="80"/>
+        <source>Length:</source>
+        <translation>Länge:</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="97"/>
+        <source>Warnings:</source>
+        <translation>Warnugen:</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="120"/>
+        <source>Comments:</source>
+        <translation>Kommentare:</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="155"/>
+        <source>Discard recording</source>
+        <translation>Aufnahme verwerfen</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="165"/>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="180"/>
+        <source>Patient type:</source>
+        <translation>Patiententyp:</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="187"/>
+        <source>Drawing hand:</source>
+        <translation>Aufnahme Hand:</translation>
+    </message>
+    <message>
+        <location filename="ui/endrecording.ui" line="200"/>
+        <source>Handedness:</source>
+        <translation>Händigkeit:</translation>
+    </message>
+</context>
+<context>
+    <name>ID</name>
+    <message>
+        <location filename="src/ID.py" line="19"/>
+        <source>Invalid Patient ID</source>
+        <translation>Ungültige Patienten ID</translation>
+    </message>
+    <message>
+        <location filename="src/ID.py" line="20"/>
+        <source>The specified patient ID is invalid</source>
+        <translation>Die angegebene Patienten ID ist ungültig</translation>
+    </message>
+    <message>
+        <location filename="src/ID.py" line="33"/>
+        <source>Invalid Tablet ID</source>
+        <translation>Ungültige Tablet ID</translation>
+    </message>
+    <message>
+        <location filename="src/ID.py" line="34"/>
+        <source>The specified tablet ID is invalid</source>
+        <translation>Die angegebene Tablet ID ist ungültig</translation>
+    </message>
+</context>
+<context>
+    <name>NewCalibrationDialog</name>
+    <message>
+        <location filename="ui/newcalibration.ui" line="26"/>
+        <source>New calibration</source>
+        <translation>Neue Kalibrierung</translation>
+    </message>
+    <message>
+        <location filename="ui/newcalibration.ui" line="35"/>
+        <source>Tablet ID:</source>
+        <translation>Tablet ID:</translation>
+    </message>
+    <message>
+        <location filename="ui/newcalibration.ui" line="45"/>
+        <source>Drawing ID:</source>
+        <translation>Aufnahme ID:</translation>
+    </message>
+    <message>
+        <location filename="ui/newcalibration.ui" line="70"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="ui/newcalibration.ui" line="80"/>
+        <source>Start calibration</source>
+        <translation>Start Kalibrierung</translation>
+    </message>
+</context>
+<context>
+    <name>NewRecordingDialog</name>
+    <message>
+        <location filename="ui/newrecording.ui" line="26"/>
+        <source>New recording</source>
+        <translation>Neue Aufnahme</translation>
+    </message>
+    <message>
+        <location filename="ui/newrecording.ui" line="35"/>
+        <source>Patient ID:</source>
+        <translation>Patienten ID:</translation>
+    </message>
+</context>
+<context>
+    <name>analysis</name>
+    <message>
+        <location filename="src/Analysis.py" line="135"/>
+        <source>Multiple strokes</source>
+        <translation>Mehrere Linien</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="144"/>
+        <source>Short recording</source>
+        <translation>Kurze Aufnahme</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="149"/>
+        <source>Old calibration data</source>
+        <translation>Kalibration veraltet</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="213"/>
+        <source>Invalid or unsupported file format</source>
+        <translation>Ungültiges oder nicht unterstütztes Dateiformat</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="138"/>
+        <source>No data!</source>
+        <translation>Keine Daten!</translation>
+    </message>
+</context>
+<context>
+    <name>calib</name>
+    <message>
+        <location filename="src/DrawingWindow.py" line="39"/>
+        <source>CALIBRATION</source>
+        <translation>KALIBRATION</translation>
+    </message>
+    <message>
+        <location filename="src/DrawingWindow.py" line="40"/>
+        <source>Place the pen on the red point and press ENTER
+Press TAB to restart, ESC to abort calibration</source>
+        <translation>Setzen Sie den Stift auf den roten Punkt  und drücken Sie ENTER
+Drücken Sie TAB um neu zu beginnen, ESC um die Kalibration abzubrechen</translation>
+    </message>
+    <message>
+        <location filename="src/DrawingWindow.py" line="76"/>
+        <source>Previewing calibration. To accept the results press ENTER</source>
+        <translation>Vorschau Kalibration. Um die Eingabe zu akzeptieren drücken Sie ENTER</translation>
+    </message>
+    <message>
+        <location filename="src/DrawingWindow.py" line="112"/>
+        <source>No cursor at point!</source>
+        <translation>Kein Cursor am Punkt!</translation>
+    </message>
+    <message>
+        <location filename="src/DrawingWindow.py" line="67"/>
+        <source>CALIBRATION FAILED: {reason}!
+Try refitting the paper. Restarting calibration...</source>
+        <translation>KALIBRATION FEHLGESCHLAGEN: {reason}!
+Versuchen Sie das Papier neu auszurichten. Starte Kalibration neu...</translation>
+    </message>
+    <message>
+        <location filename="src/DrawingWindow.py" line="106"/>
+        <source>Calibrating point {cur}/{tot}</source>
+        <translation>Kalibrationspunkt {cur}/{tot}</translation>
+    </message>
+</context>
+<context>
+    <name>drawing</name>
+    <message>
+        <location filename="src/Drawing.py" line="71"/>
+        <source>excessive rotation</source>
+        <translation>massive Rotation</translation>
+    </message>
+    <message>
+        <location filename="src/Drawing.py" line="79"/>
+        <source>excessive deformation</source>
+        <translation>massive Deformation</translation>
+    </message>
+    <message>
+        <location filename="src/Drawing.py" line="103"/>
+        <source>cannot remap coordinates</source>
+        <translation>Kann Koordinaten nicht abbilden</translation>
+    </message>
+</context>
+<context>
+    <name>mainWindow</name>
+    <message>
+        <location filename="ui/main.ui" line="26"/>
+        <source>Drawing recorder</source>
+        <translation>Drawing Recorder</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="36"/>
+        <source>Save path:</source>
+        <translation>Sicherungsverzeichnis:</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="48"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="57"/>
+        <source>Drawing ID:</source>
+        <translation>Aufnahme ID:</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="79"/>
+        <source>Calibrate</source>
+        <translation>Kalibration</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="89"/>
+        <source>New recording</source>
+        <translation>Neue Aufnahme</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="96"/>
+        <source>Quit</source>
+        <translation>Schließen</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="105"/>
+        <source>Last calibration:</source>
+        <translation>Letzte Kalibration:</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="139"/>
+        <source>TextLabel</source>
+        <translation>Beschriftung</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="122"/>
+        <source>Tablet ID</source>
+        <translation>Tablet ID</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="149"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+</context>
+<context>
+    <name>rec</name>
+    <message>
+        <location filename="src/DrawingWindow.py" line="156"/>
+        <source>RECORDING</source>
+        <translation>AUFNAHME</translation>
+    </message>
+    <message>
+        <location filename="src/DrawingWindow.py" line="157"/>
+        <source>Recording starts automatically as soon as the pen touches the tablet
+Press TAB to restart, ENTER to stop, ESC to abort recording</source>
+        <translation>Die Aufnahme startet automatisch sobald der Stift das Tablet berührt
+Drücken Sie TAB um neuzustarten, ENTER um zu stoppen, ESC um abzubrechen</translation>
+    </message>
+    <message>
+        <location filename="src/DrawingWindow.py" line="195"/>
+        <source>Waiting for events...</source>
+        <translation>Warte auf Ereignisse...</translation>
+    </message>
+    <message>
+        <location filename="src/DrawingWindow.py" line="218"/>
+        <source>recording: {recording}
+strokes: {strokes}
+events: {events}
+length: {length}</source>
+        <translation>Aufnahme: {recording}
+Linien: {strokes}
+Ereignisse: {events}
+Länge: {length}</translation>
+    </message>
+</context>
+<context>
+    <name>recorder</name>
+    <message>
+        <location filename="src/Recorder.py" line="70"/>
+        <source>Invalid drawing ID</source>
+        <translation>Ungültige Aufnahme ID</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="71"/>
+        <source>The specified drawing ID is invalid</source>
+        <translation>Die angegebene Aufbahme ID ist ungültig</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="117"/>
+        <source>Recordings</source>
+        <translation>Aufnahmen</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="169"/>
+        <source>Discard recording</source>
+        <translation>Aufnahme verwerfen</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="170"/>
+        <source>Are you sure you want to discard the acquired drawing?</source>
+        <translation>Sind Sie sicher, dass Sie die Aufnahme verwerfen möchten?</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="171"/>
+        <source>No</source>
+        <translation>Nein</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="171"/>
+        <source>Yes, discard</source>
+        <translation>Ja, verwerfen</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="349"/>
+        <source>Save failure</source>
+        <translation>Speichern fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="249"/>
+        <source>About DrawingRecorder</source>
+        <translation>Über DrawingRecorder</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="341"/>
+        <source>Saving, please wait...</source>
+        <translation>Speichern, bitte warten...</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="195"/>
+        <source>The file {path} already exists. Try with a different file name!</source>
+        <translation>Die Datei {path} existiert bereits. Geben Sie einen anderen Dateinamen an!</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="251"/>
+        <source>Total recordings: {total}
+Inst. UUID: {uuid}
+Inst. Date: {date}</source>
+        <translation>Summe Aufnahmen: {total}
+Inst. UUID: {uuid}
+Inst. Datum: {date}</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="345"/>
+        <source>Cannot save recording to {path}: {reason}! Try with a different file name!</source>
+        <translation>Kann Aufnahme nicht in {path} abspeichern: {reason}. Geben Sie einen anderen Dateinamen an!</translation>
+    </message>
+</context>
+<context>
+    <name>types</name>
+    <message>
+        <location filename="src/Analysis.py" line="95"/>
+        <source>Case</source>
+        <translation>Prüfgruppe</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="96"/>
+        <source>Control</source>
+        <translation>Kontrollgruppe</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="102"/>
+        <source>Left-handed</source>
+        <translation>Linkshänder</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="103"/>
+        <source>Right-handed</source>
+        <translation>Rechtshänder</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="104"/>
+        <source>Ambidextrous</source>
+        <translation>Beidhändig</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="109"/>
+        <source>Left</source>
+        <translation>Links</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="110"/>
+        <source>Right</source>
+        <translation>Rechts</translation>
+    </message>
+    <message>
+        <location filename="src/Recorder.py" line="25"/>
+        <source>N/A</source>
+        <translation>k.A.</translation>
+    </message>
+</context>
+</TS>
---
+++ intl/visualizer.de.ts
@@ -0,0 +1,310 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="de" sourcelanguage="">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui/visualizer.ui" line="14"/>
+        <source>Drawing visualizer</source>
+        <translation>Drawing Visualizer</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="100"/>
+        <source>Main toolbar</source>
+        <translation>Hauptmenü</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="124"/>
+        <source>Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="127"/>
+        <source>Ctrl+O</source>
+        <translation>Strg+O</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="136"/>
+        <source>Exit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="139"/>
+        <source>Ctrl+Q</source>
+        <translation>Strg+Q</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="155"/>
+        <source>RAW/Corrected</source>
+        <translation>Roh/korrigiert</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="158"/>
+        <source>Ctrl+R</source>
+        <translation>Strg+R</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="174"/>
+        <source>Speed/Time</source>
+        <translation>Geschwindigkeit/Zeit</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="177"/>
+        <source>Ctrl+P</source>
+        <translation>Strg+P</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="192"/>
+        <source>Show traces</source>
+        <translation>Kontaklose Linien an/aus</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="195"/>
+        <source>Ctrl+T</source>
+        <translation>Strg+T</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="207"/>
+        <source>Show tilt</source>
+        <translation>Neigung an/aus</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="210"/>
+        <source>Ctrl+I</source>
+        <translation>Strg+I</translation>
+    </message>
+    <message>
+        <location filename="ui/visualizer.ui" line="222"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+</context>
+<context>
+    <name>analysis</name>
+    <message>
+        <location filename="src/Analysis.py" line="135"/>
+        <source>Multiple strokes</source>
+        <translation>Mehrere Linien</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="144"/>
+        <source>Short recording</source>
+        <translation>Kurze Aufnahme</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="149"/>
+        <source>Old calibration data</source>
+        <translation>Kalibration veraltet</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="213"/>
+        <source>Invalid or unsupported file format</source>
+        <translation>Ungültiges oder nicht unterstütztes Dateiformat</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="138"/>
+        <source>No data!</source>
+        <translation>Keine Daten!</translation>
+    </message>
+</context>
+<context>
+    <name>types</name>
+    <message>
+        <location filename="src/Visualizer.py" line="84"/>
+        <source>N/A</source>
+        <translation>k.A.</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="95"/>
+        <source>Case</source>
+        <translation>Prüfgruppe</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="96"/>
+        <source>Control</source>
+        <translation>Kontrollgruppe</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="102"/>
+        <source>Left-handed</source>
+        <translation>Linkshänder</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="103"/>
+        <source>Right-handed</source>
+        <translation>Rechtshänder</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="104"/>
+        <source>Ambidextrous</source>
+        <translation>Beidhändig</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="109"/>
+        <source>Left</source>
+        <translation>Links</translation>
+    </message>
+    <message>
+        <location filename="src/Analysis.py" line="110"/>
+        <source>Right</source>
+        <translation>Rechts</translation>
+    </message>
+</context>
+<context>
+    <name>visualizer</name>
+    <message>
+        <location filename="src/Visualizer.py" line="68"/>
+        <source>Property</source>
+        <translation>Eigenschaft</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="68"/>
+        <source>Value</source>
+        <translation>Wert</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="92"/>
+        <source>Pat. ID</source>
+        <translation>Pat. ID</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="94"/>
+        <source>Pat. type</source>
+        <translation>Pat. Typ</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="96"/>
+        <source>Pat. handedness</source>
+        <translation>Pat. Händigkeit</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="98"/>
+        <source>Pat. hand</source>
+        <translation>Pat. Hand</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="101"/>
+        <source>Drawing ID</source>
+        <translation>Aufbahme ID</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="103"/>
+        <source>Drawing descr.</source>
+        <translation>Aufnahme Besch.</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="106"/>
+        <source>Rec. date</source>
+        <translation>Aufnahmedatum</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="108"/>
+        <source>Rec. no.</source>
+        <translation>Aufnahmenr.</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="110"/>
+        <source>Rec. retries</source>
+        <translation>Aufnahme Wiederholungen</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="112"/>
+        <source>Rec. strokes</source>
+        <translation>Aufnahme Linien</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="114"/>
+        <source>Rec. events</source>
+        <translation>Aufnahme Ereignisse</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="116"/>
+        <source>Rec. start</source>
+        <translation>Aufnahme Start</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="118"/>
+        <source>Rec. length</source>
+        <translation>Aufnahme Länge</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="122"/>
+        <source>Calib. tablet ID</source>
+        <translation>Kalib. Tablet ID</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="124"/>
+        <source>Calib. date</source>
+        <translation>Kalib. Datum</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="126"/>
+        <source>Calib. age</source>
+        <translation>Kalib. Nutzungsdauer</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="129"/>
+        <source>Screen width</source>
+        <translation>Bildschirmbreite</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="131"/>
+        <source>Screen height</source>
+        <translation>Bildschirmhöhe</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="134"/>
+        <source>Software version</source>
+        <translation>Softwareversion</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="136"/>
+        <source>Format version</source>
+        <translation>Formatversion</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="139"/>
+        <source>Inst. UUID</source>
+        <translation>Inst. UUID</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="141"/>
+        <source>Inst. date</source>
+        <translation>Inst. Datum</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="143"/>
+        <source>Inst. recordings</source>
+        <translation>Inst. Aufnahmen</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="315"/>
+        <source>Loading, please wait...</source>
+        <translation>Ladevorgang, bitte warten...</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="322"/>
+        <source>Load failure</source>
+        <translation>Fehler beim Laden</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="331"/>
+        <source>Load recording</source>
+        <translation>Lade Aufnahme</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="332"/>
+        <source>Recordings</source>
+        <translation>Aufnahmen</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="341"/>
+        <source>About DrawingVisualizer</source>
+        <translation>Über DrawingVisualizer</translation>
+    </message>
+    <message>
+        <location filename="src/Visualizer.py" line="319"/>
+        <source>Cannot load recording {path}: {reason}</source>
+        <translation>Kann Aufnahme nicht laden {path}: {reason}</translation>
+    </message>
+</context>
+</TS>
