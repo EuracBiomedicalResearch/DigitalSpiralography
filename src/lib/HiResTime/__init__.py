e869f616548721d297ced0a2f37f99140be5a482
Yuri D'Elia <yuri.delia@eurac.edu>
2015-04-24 15:20:14 +0200

    Raw monotonic time support also in Linux.

--- src/lib/HiResTime/__init__.py
+++ src/lib/HiResTime/__init__.py
@@ -5,13 +5,57 @@ import datetime
 import sys
 import time
 
-if sys.platform != 'win32':
-    # On Posix systems time() calls gettimeofday() and is an hi-res wall clock
-    now = datetime.datetime.now
-else:
+__all__ = ('resync', 'now')
+
+
+if sys.platform == 'win32':
     # On Win32 time() has only sub-second resolution. Use clock() instead,
-    # which is actually sub-ms resolution wall clock.
+    # which is a sub-ms resolution, raw monotonic clock.
     __ts_init = time.time() - time.clock()
+
+    def resync():
+        __ts_init = time.time() - time.clock()
 
     def now():
         return datetime.datetime.fromtimestamp(__ts_init + time.clock())
+
+
+elif sys.platform == 'linux2':
+    # On Linux we use CLOCK_MONOTONIC_RAW though the clock_gettime() function.
+    import ctypes, os, time
+
+    CLOCK_MONOTONIC_RAW = 4
+
+    class timespec(ctypes.Structure):
+        _fields_ = (
+            ('tv_sec', ctypes.c_long),
+            ('tv_nsec', ctypes.c_long),
+    )
+
+    librt = ctypes.CDLL('librt.so.1', use_errno=True)
+    _clock_gettime = librt.clock_gettime
+    _clock_gettime.argtypes = (ctypes.c_int, ctypes.POINTER(timespec))
+
+    def clock_gettime():
+        t = timespec()
+        if _clock_gettime(CLOCK_MONOTONIC_RAW, ctypes.byref(t)) != 0:
+            errno_ = ctypes.get_errno()
+            raise OSError(errno_, os.strerror(errno_))
+        return t.tv_sec + t.tv_nsec * 1e-9
+
+    __ts_init = time.time() - clock_gettime()
+
+    def resync():
+        __ts_init = time.time() - clock_gettime()
+
+    def now():
+        return datetime.datetime.fromtimestamp(__ts_init + clock_gettime())
+
+
+else:
+    # On Posix systems time() calls gettimeofday() and is an hi-res wall clock,
+    # but is not monotonic and is affected by adjtime.
+    now = datetime.datetime.now
+
+    def resync():
+        pass
